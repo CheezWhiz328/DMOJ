cbccb0c6e346a1386799bfc07ed378d9ee31905b
David-Shen328 <48096367+David-Shen328@users.noreply.github.com>
2019-07-29 18:50:09 -0400

    f

--- Problems/src/APaintingProblem.java
+++ Problems/src/APaintingProblem.java
@@ -1,16 +1,24 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class PaintingProblem {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int M = sc.nextInt();
-		int K = sc.nextInt();
-		int B = 0;
-		for(int i=0; i<K; i++) {
-			
+public class APaintingProblem {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int blue = 0;
+		int i = K;
+		while(--i>=0){
+			boolean a = ((N >> i) & 1) == 1 ? true : false;
+			boolean b = ((M >> i) & 1) == 1 ? true : false;
+			if (a ^ b) {
+				blue++;
+			}
+
 		}
-	
+		System.out.println(blue + " " + (K - blue));
 
 	}
 }
