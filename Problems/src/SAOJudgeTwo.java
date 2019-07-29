9f5d50922db00b697f0e7584c18d8990dcc1e4d1
David-Shen328 <48096367+David-Shen328@users.noreply.github.com>
2019-07-28 21:24:03 -0400

    dumb problem

--- Problems/src/SAOJudgeTwo.java
+++ Problems/src/SAOJudgeTwo.java
@@ -1,13 +1,22 @@
-import java.math.BigInteger;
 import java.util.Scanner;
 
 public class SAOJudgeTwo {
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		BigInteger N = BigInteger.valueOf(sc.nextInt());
-		System.out.println(((N.multiply(N)).add(N)).divide(BigInteger.valueOf(2)));
-		
+		long n = sc.nextLong();
+		long ans = (n*(n+1))/2;
+		System.out.println("#include <iostream>\r\n" + 
+				"\r\n" + 
+				"using namespace std;\r\n" + 
+				"\r\n" + 
+				"int main()\r\n" + 
+				"{\r\n" + 
+				"    \r\n" + 
+				"    cout << "+ans+";\r\n" + 
+				"\r\n" + 
+				"    return 0;\r\n" + 
+				"}\r\n" + 
+				"");
 	}
 }
