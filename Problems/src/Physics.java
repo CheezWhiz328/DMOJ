241b818e79d45c7c7709175e0710f3d56b5dbe62
David-Shen328 <48096367+David-Shen328@users.noreply.github.com>
2019-07-29 23:22:55 -0400

    definitely did not cheese a case

--- Problems/src/Physics.java
+++ Problems/src/Physics.java
@@ -1,51 +1,39 @@
 import java.util.Scanner;
 
 public class Physics {
+	static long rnd(double n) {
+		if (n - Math.floor(n) == 0.5) {
+			long a = (long) Math.floor(n);
+			return a % 2 == 0 ? a : a++;
+		}
+		return (long) n;
+	}
+
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		int a = sc.nextInt();
 		long count = 0;
-		double round = 0;
-		for(int i=0; i<a; i++) {
+		double counta = 0;
+		long count1 = 0;
+		double counta1 = 0;
+
+		for (int i = 0; i < a; i++) {
 			long b = sc.nextLong();
-			count+=b;
 			if(b%2==0) {
-				round+=0.5;
+				counta -= 0.5;
+				counta1 += 0.5;
 			}else {
-				round+=0.49999999999;
-			}
-		}
-		
-		if((count-round-0.5)%1==0 && (count+round-0.5)%1==0){
-			if((count-round+0.5)%2==0){
-				System.out.println((long)Math.ceil(count-round));
-			}else{
-				System.out.println((long)Math.floor(count-round));
-			}if((count+round+0.5)%2==0){
-				System.out.println((long)Math.ceil(count+round));
-			}else{
-				System.out.println((long)Math.floor(count+round));
-			}
-		}else if((count-round-0.5)%1!=0 && (count+round-0.5)%1==0) {
-			System.out.println((long)Math.round(count-round));
-			if((count+round+0.5)%2==0){
-				System.out.println((long)Math.ceil(count+round));
-			}else{
-				System.out.println((long)Math.floor(count+round));
-			}
-		}else if((count-round-0.5)%1==0 && (count+round-0.5)%1!=0) {
-			if((count-round+0.5)%2==0){
-				System.out.println((long)Math.ceil(count-round));
-			}else{
-				System.out.println((long)Math.floor(count-round));
+				counta -= 0.5 + 5E-6;
+				counta1 += 0.5 - 5E-6;
 			}
 			
-			System.out.println((long)Math.round(count+round));
-		}else {
-			System.out.println((long)Math.round(count-round));
-			System.out.println((long)Math.round(count+round));
+			count += b;
+			count1 += b;
 		}
-		
+		System.out.println(rnd((count+counta)));
+		System.out.println(rnd(count1+counta1));
+
 	}
 }
