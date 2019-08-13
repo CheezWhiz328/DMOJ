10828518a63d29d9bda886d637313952f99eb057
David-Shen328 <48096367+David-Shen328@users.noreply.github.com>
2019-08-13 19:09:43 -0400

    dmoj

--- Problems/src/MimiAndPrimes.java
+++ Problems/src/MimiAndPrimes.java
@@ -1,57 +1,149 @@
-import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
 
 public class MimiAndPrimes {
+	static long gcd(long a, long b) {
+		if (a == 0)
+			return b;
+		return gcd(b % a, a);
+	}
+
+	static long findGCD(long arr[]) {
+		long result = arr[0];
+		for (int i = 1; i < arr.length; i++) {
+			result = gcd(arr[i], result);
+
+		}
+
+		while(result % 2 == 0 && result != 2) {
+			result /= 2;
+		}
+		for (long i = 3; i * i <= result; i++) {
+			while (result % i == 0 && result != i) {
+				result /= i;
+			}
+		}
+
+		if (result == 4) {
+			return 2;
+		}
+		return result;
+	}
+
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int a = Integer.parseInt(br.readLine());
-		LinkedList<Long> prime = new LinkedList<Long>();
-
-		String[] tmp = br.readLine().split(" ");
-		Long b = Long.parseLong(tmp[0]);
-		if (b == 1) {
-			System.out.println("DNE");
-			System.exit(0);
+		Reader sc = new Reader();
+		int a = sc.nextInt();
+		long arr[] = new long[a];
+		for (int i = 0; i < a; i++) {
+			arr[i] = sc.nextLong();
 		}
-		if (b % 2 == 0) {
-			prime.add((long) 2);
+		long n = findGCD(arr);
+		System.out.println(n==1 ? "DNE" : n);
+	}
+
+	static class Reader {
+		final private int BUFFER_SIZE = 1 << 16;
+		private DataInputStream din;
+		private byte[] buffer;
+		private int bufferPointer, bytesRead;
+
+		public Reader() {
+			din = new DataInputStream(System.in);
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
 		}
 
-		for (long i = 3; Math.sqrt(i) < b; i += 2) {
-			if (b % i == 0) {
-				while (b % i == 0) {
-					b /= i;
+		public Reader(String file_name) throws IOException {
+			din = new DataInputStream(new FileInputStream(file_name));
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
+		}
+
+		public String readLine() throws IOException {
+			byte[] buf = new byte[64]; // line length
+			int cnt = 0, c;
+			while ((c = read()) != -1) {
+				if (c == '\n')
+					break;
+				buf[cnt++] = (byte) c;
+			}
+			return new String(buf, 0, cnt);
+		}
+
+		public int nextInt() throws IOException {
+			int ret = 0;
+			byte c = read();
+			while (c <= ' ')
+				c = read();
+			boolean neg = (c == '-');
+			if (neg)
+				c = read();
+			do {
+				ret = ret * 10 + c - '0';
+			} while ((c = read()) >= '0' && c <= '9');
+
+			if (neg)
+				return -ret;
+			return ret;
+		}
+
+		public long nextLong() throws IOException {
+			long ret = 0;
+			byte c = read();
+			while (c <= ' ')
+				c = read();
+			boolean neg = (c == '-');
+			if (neg)
+				c = read();
+			do {
+				ret = ret * 10 + c - '0';
+			} while ((c = read()) >= '0' && c <= '9');
+			if (neg)
+				return -ret;
+			return ret;
+		}
+
+		public double nextDouble() throws IOException {
+			double ret = 0, div = 1;
+			byte c = read();
+			while (c <= ' ')
+				c = read();
+			boolean neg = (c == '-');
+			if (neg)
+				c = read();
+
+			do {
+				ret = ret * 10 + c - '0';
+			} while ((c = read()) >= '0' && c <= '9');
+
+			if (c == '.') {
+				while ((c = read()) >= '0' && c <= '9') {
+					ret += (c - '0') / (div *= 10);
 				}
-				prime.add(i);
 			}
+
+			if (neg)
+				return -ret;
+			return ret;
 		}
 
-		for (int i = 1; i < a; i++) {
-			long c = Long.parseLong(tmp[i]);
-			if(b%c==0) {
-				break;
-			}
-
-			if (c == 1) {
-				System.out.println("DNE");
-				System.exit(0);
-			}
-			int count = 0;
-			for (int j = 0; j < prime.size(); j++) {
-				if (c % prime.get(j - count) != 0) {
-					prime.remove(j);
-					j--;
-				}
-			}
-			if (prime.isEmpty()) {
-				System.out.println("DNE");
-				System.exit(0);
-			}
+		private void fillBuffer() throws IOException {
+			bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+			if (bytesRead == -1)
+				buffer[0] = -1;
 		}
-		System.out.println(prime.get(prime.size() - 1));
 
+		private byte read() throws IOException {
+			if (bufferPointer == bytesRead)
+				fillBuffer();
+			return buffer[bufferPointer++];
+		}
+
+		public void close() throws IOException {
+			if (din == null)
+				return;
+			din.close();
+		}
 	}
 }
